b053c77cb3c11229d21fecd2471a4973ff3825ae
egorova-sofya <EgorovaSjob@yandex.ru>
2023-02-26 13:27:37 +0300

    fixed svg icon attributes

---
+++ components/Icons/Arrow.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Arrow = () => {
+  return (
+    <>
+      <svg
+        width="10"
+        height="19"
+        viewBox="0 0 10 19"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 1L9 9.5L1 18"
+          stroke="#1A231F"
+          strokeWidth="2"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default Arrow;
